73603b1fa4b141633b8e455f1ef1c57a3130573f
mariam-90 <129694311+maryam19455@users.noreply.github.com>
2024-02-20 22:14:09 +0200

    SMAR-2
    Signup

---
+++ server/routes/user.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const { User } = require("../models/user");
+const { json } = require("express");
+
+router.get("/email/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+    console.log(email);
+    let user = await User.findOne({ email });
+    user = user.toObject();
+    delete user.password;
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.put("/id/:id", async (req, res) => {
+  try {
+    const { firstName, lastName } = req.body;
+    const user = await User.findById(req.params.id);
+    if (!user)
+      return res
+        .status(404)
+        .send({ message: "User with given ID doesn't exist!" });
+    user.firstName = firstName;
+    user.lastName = lastName;
+    await user.save();
+    res.status(200).send({ message: "User updated successfully", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+
+
+module.exports = router;
