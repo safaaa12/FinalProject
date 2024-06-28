47628ef96f9c866e133cd2db8dd284900f70f884
Mariam-90 <83160379+Mariam-90@users.noreply.github.com>
2024-06-28 17:20:15 +0300

    SMAR-21
    Displaying the data on the page.
    Ability to add and remove a user as a nutritionist

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -1,20 +1,60 @@
 import "./styles.css";
 import React, { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from "react-router-dom"; // שימוש ב-useNavigate
+import Alert from 'react-bootstrap/Alert';
 import axios from 'axios';
+import AdminManagement from "./adminMangment";
 
 function Profile() {
+    const [data, setData] = useState({});
+    const navigate = useNavigate(); // יצירת אינסטנס של useNavigate
+
     const [user, setUser] = useState(null);
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setemail] = useState("");
     const [isTzunai, setTzunai] = useState("");
+    const [isAdmin, setAdmin] = useState("");
     const [password, setpassword] = useState("");
     const [loction, setUserId] = useState(null);
+    const [articleSuccess, setArticleSuccess] = useState("");
+    const [recipeSuccess, setRecipeSuccess] = useState("");
+
 
     useEffect(() => {
         const userEmail = localStorage.getItem('email');
         fetchUser(userEmail);
     }, []);
+
+    const handleArticleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const formDataobj = Object.fromEntries(formData.entries());
+        formDataobj.tzunaiName = firstName + " " + lastName;
+        try {
+            const url = "http://localhost:3000/api/article/add";
+            const { data: res } = await axios.post(url, formDataobj);
+            setArticleSuccess(true);
+            e.target.reset();
+        } catch (error) {
+        }
+    };
+
+    const handleRecipeSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const formDataobj = Object.fromEntries(formData.entries());
+        formDataobj.tzunaiName = firstName + " " + lastName;
+        try {
+            const url = "http://localhost:3000/api/recipe/add";
+            const { data: res } = await axios.post(url, formDataobj);
+            setRecipeSuccess(true);
+            e.target.reset();
+        } catch (error) {
+        }
+    };
 
     async function fetchUser(userEmail) {
         const urlEncodedEmail = encodeURIComponent(userEmail);
@@ -25,12 +65,14 @@ function Profile() {
             setLastName(response.data.lastName);
             setemail(response.data.email);
             setTzunai(response.data.isTzunai);
+            setAdmin(response.data.isAdmin);
             setpassword(response.data.email);
             setUserId(response.data.loction);
         } catch (error) {
             console.log("fetchUser: ", error);
         }
     }
+
 
     return (
         <>
@@ -53,14 +95,14 @@ function Profile() {
           {/* Add your content here */}
           <p>Tzunai is true!</p>
         </>
-      ) : (
-        <>
-          {/* Add content for the false case if needed */}
-          <p>Tzunai is false!</p>
-        </>
-      )}
+      ) :  (
+        <></>)
+    }
+    {isAdmin ? (<>
+        <AdminManagement />
+    </>) : (<></>)}
 
-            </>
+</>
     )
 
 }
